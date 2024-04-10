810e1bf74d5907efc280a12f9ece810765c28558
jiawei-zhang-a <jz4721@nyu.edu>
2024-04-10 14:36:03 -0400

    MNT: adjust the default method

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="python-iArt",
-    version="0.1.4",
+    version="1.0.2",
     author="Siyu Heng, Jiawei Zhang, and Yang Feng",
     author_email="siyuheng@nyu.edu,jz4721@nyu.edu,yang.feng@nyu.edu",
     description="iArt: A Generalized Framework for Imputation-Assisted Randomization Tests",
